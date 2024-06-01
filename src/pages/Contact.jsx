a846b2e27502d2c8dea242b4149b5dc6dbe65988
Abuubakar Ciise <138715168+Abuubakar-Ciise@users.noreply.github.com>
2024-06-02 07:13:02 +1200

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,8 +5,6 @@ const Contact = () => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
-    phone: "",
-    company: "",
     message: ""
   });
 
@@ -21,14 +19,19 @@ const Contact = () => {
     e.preventDefault();
 
     emailjs.send(
-      "service_htpcuxs", // Replace with your EmailJS service ID
-      "template_2g34rpg", // Replace with your EmailJS template ID
+      "service_htpcuxs", 
+      "template_2g34rpg",
       formData,
-      "FbwINVHqM10th-DC9" // Replace with your EmailJS user ID (also called Public Key)
+      "FbwINVHqM10th-DC9" 
     ).then(
       (result) => {
         console.log(result.text);
         alert("Message Sent Successfully!");
+        setFormData({
+          name: "",
+          email: "",
+          message: ""
+        });
       },
       (error) => {
         console.log(error.text);
@@ -37,6 +40,7 @@ const Contact = () => {
     );
   };
 
+
   return (
     <section className="py-10 bg-gray-100 sm:py-16 lg:py-24">
       <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
@@ -44,49 +48,15 @@ const Contact = () => {
           <h2 className="text-3xl font-bold leading-tight text-gray-900 sm:text-4xl lg:text-5xl">Contact us</h2>
           <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-500">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</p>
         </div>
-        <div className="max-w-5xl mx-auto mt-12 sm:mt-16">
-          <div className="grid grid-cols-1 gap-6 px-8 text-center md:px-0 md:grid-cols-3">
+        <div className="max-w-lg mx-auto mt-12 sm:mt-16">
+          <div className="mx-auto">
             <div className="overflow-hidden bg-white rounded-xl">
-              <div className="p-6">
-                <svg className="flex-shrink-0 w-10 h-10 mx-auto text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1"
-                    d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                  />
-                </svg>
-                <p className="mt-6 text-lg font-medium text-gray-900">+1-316-555-0116</p>
-                <p className="mt-1 text-lg font-medium text-gray-900">+1-446-526-0117</p>
-              </div>
-            </div>
-            <div className="overflow-hidden bg-white rounded-xl">
-              <div className="p-6">
-                <svg className="flex-shrink-0 w-10 h-10 mx-auto text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                </svg>
-                <p className="mt-6 text-lg font-medium text-gray-900">contact@example.com</p>
-                <p className="mt-1 text-lg font-medium text-gray-900">hr@example.com</p>
-              </div>
-            </div>
-            <div className="overflow-hidden bg-white rounded-xl">
-              <div className="p-6">
-                <svg className="flex-shrink-0 w-10 h-10 mx-auto text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
-                <p className="mt-6 text-lg font-medium leading-relaxed text-gray-900">8502 Preston Rd. Ingle, Maine 98380, USA</p>
-              </div>
-            </div>
-          </div>
-          <div className="mt-6 overflow-hidden bg-white rounded-xl">
-            <div className="px-6 py-12 sm:p-12">
-              <h3 className="text-3xl font-semibold text-center text-gray-900">Send us a message</h3>
-              <form onSubmit={handleSubmit} className="mt-14">
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-4">
+              <div className="p-8">
+                <h3 className="text-3xl font-semibold text-center text-gray-900">Send us a message</h3>
+                <form onSubmit={handleSubmit} className="mt-8">
                   <div>
-                    <label htmlFor="name" className="text-base font-medium text-gray-900"> Your name </label>
-                    <div className="mt-2.5 relative">
+                    <label htmlFor="name" className="block text-base font-medium text-gray-900">Your name</label>
+                    <div className="mt-2 relative">
                       <input
                         type="text"
                         name="name"
@@ -95,12 +65,13 @@ const Contact = () => {
                         onChange={handleChange}
                         placeholder="Enter your full name"
                         className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
+                        required
                       />
                     </div>
                   </div>
-                  <div>
-                    <label htmlFor="email" className="text-base font-medium text-gray-900"> Email address </label>
-                    <div className="mt-2.5 relative">
+                  <div className="mt-6">
+                    <label htmlFor="email" className="block text-base font-medium text-gray-900">Your email address</label>
+                    <div className="mt-2 relative">
                       <input
                         type="email"
                         name="email"
@@ -109,40 +80,13 @@ const Contact = () => {
                         onChange={handleChange}
                         placeholder="Enter your email"
                         className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
+                        required
                       />
                     </div>
                   </div>
-                  <div>
-                    <label htmlFor="phone" className="text-base font-medium text-gray-900"> Phone number </label>
-                    <div className="mt-2.5 relative">
-                      <input
-                        type="tel"
-                        name="phone"
-                        id="phone"
-                        value={formData.phone}
-                        onChange={handleChange}
-                        placeholder="Enter your phone number"
-                        className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <label htmlFor="company" className="text-base font-medium text-gray-900"> Company name </label>
-                    <div className="mt-2.5 relative">
-                      <input
-                        type="text"
-                        name="company"
-                        id="company"
-                        value={formData.company}
-                        onChange={handleChange}
-                        placeholder="Enter your company name"
-                        className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600"
-                      />
-                    </div>
-                  </div>
-                  <div className="sm:col-span-2">
-                    <label htmlFor="message" className="text-base font-medium text-gray-900"> Message </label>
-                    <div className="mt-2.5 relative">
+                  <div className="mt-6">
+                    <label htmlFor="message" className="block text-base font-medium text-gray-900">Your message</label>
+                    <div className="mt-2 relative">
                       <textarea
                         name="message"
                         id="message"
@@ -151,24 +95,26 @@ const Contact = () => {
                         placeholder="Enter your message"
                         className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md resize-y focus:outline-none focus:border-blue-600 caret-blue-600"
                         rows="4"
+                        required
                       />
                     </div>
                   </div>
-                  <div className="sm:col-span-2">
+                  <div className="mt-8">
                     <button
                       type="submit"
-                      className="inline-flex items-center justify-center w-full px-4 py-4 mt-2 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md focus:outline-none hover:bg-blue-700 focus:bg-blue-700"
+                      className="inline-flex items-center justify-center w-full px-6 py-3 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md focus:outline-none hover:bg-blue-700 focus:bg-blue-700"
                     >
-                      Send
+                      Send Message
                     </button>
                   </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </section>
+
   );
 };
 
